d42a4ae6174d358a1160c25081259aef9d8a45d3
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-10-15 12:17:15 +0300

    Structure files added

---
+++ instance/config.py
@@ -0,0 +1,4 @@
+SECRET_KEY="flasksecret"
+
+MAIL_USERNAME="donatello54611@gmail.com"
+MAIL_PASSWORD="halowars5611"
---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY="flasksecret"
+
+export MAIL_USERNAME="donatello54611@gmail.com"
+export MAIL_PASSWORD="halowars5611"
+
+python3.6 manage.py server
